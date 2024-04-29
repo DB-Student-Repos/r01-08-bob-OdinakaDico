6eebeaeeb05625c90149dcd319035a6333cfdc91
OdinakaDico <149073751+OdinakaDico@users.noreply.github.com>
2024-04-29 16:01:31 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {message}")
+   let message = message.trim();
+
+    if message.is_empty() {
+        "Fine. Be that way!"
+    } else if message.chars().all(|c| c.is_ascii_uppercase()) && message.contains('?') {
+        "Calm down, I know what I'm doing!"
+    } else if message.chars().all(|c| c.is_ascii_uppercase()) {
+        "Whoa, chill out!"
+    } else if message.ends_with('?') {
+        "Sure."
+    } else {
+        "Whatever."
+    }
 }
